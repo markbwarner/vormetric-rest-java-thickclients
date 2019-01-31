62bb89f0bdbc6bea2288ff6c9e3f0aa1743e88ca
markbwarner <markbwarner@gmail.com>
2019-01-31 15:29:49 -0500

    add new jar and batch files

---
+++ run-vae-batch-demo-git.bat
@@ -0,0 +1,9 @@
+set vcsdebug=1
+SET vcsuserid=vtsroot
+echo %vcsuserid%
+set vcspassword=yourvcspwd
+set vcstokenserver=192.168.159.141
+set vcsalg=A128CTR
+set vcsivnumber=0123456789012345
+set vcsencryptdecryptkey=firstkeyviarest128
+start JavaVAEUI.jar
---
+++ run-vae-demo-git.bat
@@ -0,0 +1,13 @@
+set vcsdebug=1
+SET vcsuserid=vtsroot
+set dsmuserid=superadmin
+set dsmpwd=yourdsmpwd
+set dsmip=192.168.159.130
+
+echo %vcsuserid%
+set vcspassword=yourvcspwd
+set vcstokenserver=192.168.159.141
+set vcsalg=A128CTR
+set vcsivnumber=0123456789012345
+set vcsencryptdecryptkey=firstkeyviarest128
+start JavaVAEFX.jar
